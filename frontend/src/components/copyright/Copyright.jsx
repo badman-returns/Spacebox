a043860cb2cdaafd386c5ca5984185044e32fefa
tsgoswami <trishnangshugoswami@yahoo.in>
2021-09-21 03:05:40 +0530

    update: copyright update

--- frontend/src/components/copyright/Copyright.jsx
+++ frontend/src/components/copyright/Copyright.jsx
@@ -7,7 +7,7 @@ export default function Copyright() {
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
         <Link color="inherit" href="/">
-          Easy
+          Spacebox
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
